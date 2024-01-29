664e2a921aa9cf9ab9cd78fe0d18bc01de34f244
Your Name <you@example.com>
2024-01-29 13:02:33 -0500

    Init commit

---
+++ constants.js
@@ -0,0 +1,3 @@
+exports.SUBJECT_TABLE_CSS_SELECTOR = "#pbid-subjListTable-tb span > a";
+exports.COURSE_TABLE_CSS_SELECTOR = "#pbid-courseListTable-tb span > a";
+exports.SPAN_CONTAINER_SELECTOR = "#pbid-courseListSectionDetailSections-0"
